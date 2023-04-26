00c63389ea6c717193e6e75298bb388eb4bf196c
wuan1 <wuan1250276415@gmail.com>
2023-04-26 22:36:48 +0800

    更换了配置

--- src/main/java/pro/wuan/backendbaseplatform/controller/MyController.java
+++ src/main/java/pro/wuan/backendbaseplatform/controller/MyController.java
@@ -2,6 +2,7 @@ package pro.wuan.backendbaseplatform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import pro.wuan.backendbaseplatform.service.MyService;
@@ -18,9 +19,9 @@ public class MyController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping("/call-my-service")
-    public String callMyService() {
-        return restTemplate.getForObject("http://backendbaseplatform/services", String.class);
+    @GetMapping("/call-my-service/{services}")
+    public String callMyService(@PathVariable String services) {
+        return restTemplate.getForObject("http://backendbaseplatform/services/"+services, String.class);
     }
 
     @GetMapping("myservice/test")
